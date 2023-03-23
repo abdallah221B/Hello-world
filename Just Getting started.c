b85c8c96a8e1ecb02b32b9bbeacf48d6e1267427
abdallah221B <125769935+abdallah221B@users.noreply.github.com>
2023-03-23 03:08:16 +0200

    Just Getting started

---
+++ Just Getting started.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONTDC_NO_WARNINNGS
+#include<stdio.h>
+
+int main()
+{
+	
+	printf("Abdallah \nShehata\n");
+	printf("\"abdallah Shehata\"\n");
+	printf("\tabdallah Shehata\t Abdallah Shehata is the greatest programmer ever ");
+}
